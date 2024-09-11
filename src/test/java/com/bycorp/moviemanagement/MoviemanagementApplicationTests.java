036458357450c6ae4feb4ce3a6d9664269b25df8
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2024-09-11 13:37:19 -0500

    creacion inicial del proyecto

---
+++ src/test/java/com/bycorp/moviemanagement/MoviemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bycorp.moviemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviemanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
