1d7ad0f87e30ca5b666ef492d08b47e82b5ae624
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2024-09-11 13:36:58 -0500

    creando una enumeracion para los generos de las peliculas

---
+++ src/main/java/com/bycorp/moviemanagement/utils/MovieGenre.java
@@ -0,0 +1,19 @@
+package com.bycorp.moviemanagement.utils;
+
+public enum MovieGenre {
+    ACTION,
+    COMEDY,
+    DRAMA,
+    HORROR,
+    ROMANCE,
+    SCIENCE_FICTION,
+    FANTASY,
+    THRILLER,
+    ANIMATION,
+    DOCUMENTARY,
+    ADVENTURE,
+    CRIME,
+    MYSTERY,
+    FAMILY,
+    MUSIC
+}
