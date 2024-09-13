19f1993732b1a4fb463da0b0f32143dbea01e408
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2024-09-13 13:46:52 -0500

    cambiando el valor de retorno del findById de Optional a User

--- src/main/java/com/bycorp/moviemanagement/services/UserService.java
+++ src/main/java/com/bycorp/moviemanagement/services/UserService.java
@@ -3,12 +3,11 @@ package com.bycorp.moviemanagement.services;
 import com.bycorp.moviemanagement.entity.User;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface UserService {
     List<User> findAll();
     List<User> findByNameContaining(String name);
-    Optional<User> findByUsername(String username);
+    User findByUsername(String username);
     User findOneById(Long id);
     User createOne(User user);
     User updateOneById(Long id,User user);
--- src/main/java/com/bycorp/moviemanagement/services/impl/UserServiceImpl.java
+++ src/main/java/com/bycorp/moviemanagement/services/impl/UserServiceImpl.java
@@ -8,7 +8,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -30,8 +29,9 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    public Optional<User> findByUsername(String username) {
-        return userRepository.findByUsername(username);
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> new ObjectNotFoundException("Username: "+username+" not found"));
     }
 
     @Override
