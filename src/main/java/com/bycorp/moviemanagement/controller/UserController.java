2762355c4273a5e21f014eb8b37ab9de379c83b6
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2024-09-18 11:50:49 -0500

    creando metodo POST: funcion createUser

--- src/main/java/com/bycorp/moviemanagement/controller/UserController.java
+++ src/main/java/com/bycorp/moviemanagement/controller/UserController.java
@@ -3,11 +3,13 @@ package com.bycorp.moviemanagement.controller;
 import com.bycorp.moviemanagement.entity.User;
 import com.bycorp.moviemanagement.exception.ObjectNotFoundException;
 import com.bycorp.moviemanagement.services.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -39,5 +41,16 @@ public class UserController {
         } catch (ObjectNotFoundException e){
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<User> createUser(@RequestBody User user, HttpServletRequest request){
+        User userCreated = userService.createOne(user);
+        String baseUrl = request.getRequestURL().toString();
+        URI newLocation = URI.create(baseUrl + "/" + userCreated.getId());
+
+        return ResponseEntity
+                .created(newLocation)
+                .body(userCreated);
     }
 }
