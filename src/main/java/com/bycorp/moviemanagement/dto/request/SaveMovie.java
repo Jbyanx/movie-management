707b46185e9f9da883dedcdc33d5b973a0971264
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2024-10-20 18:12:48 -0500

    agregando validaciones de Jakarta y usando mensajes como propiedades

--- src/main/java/com/bycorp/moviemanagement/dto/request/SaveMovie.java
+++ src/main/java/com/bycorp/moviemanagement/dto/request/SaveMovie.java
@@ -2,14 +2,25 @@ package com.bycorp.moviemanagement.dto.request;
 
 import com.bycorp.moviemanagement.utils.MovieGenre;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.*;
 
 import java.io.Serializable;
 
 public record SaveMovie(
-        String title,
+        @Size(min = 4, max = 255, message = "{generic.size}")
+        @NotBlank(message = "{generic.notBlank}")
+        String title, // null - "" - "     "
+        @Size(min = 4, max = 255, message = "{generic.size}")
+        @NotBlank(message = "{generic.notBlank}")
         String director,
         MovieGenre genre,
-        @JsonProperty(value = "release_year") int releaseYear
+        @Min(value = 1900, message = "{generic.min}")
+        @JsonProperty(value = "release_year")
+        int releaseYear
+//        @JsonProperty(value = "availability_end_time")
+//        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+//        @Future
+//        LocalDateTime availabilityEndTime
 ) implements Serializable {
 
 }
