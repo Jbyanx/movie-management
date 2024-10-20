cc5fe7f20f58c927f4d75e3fee92a884eab4cdb8
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2024-10-20 17:04:56 -0500

    creando componente para validar contraseñas

---
+++ src/main/java/com/bycorp/moviemanagement/services/validator/PasswordValidator.java
@@ -0,0 +1,49 @@
+package com.bycorp.moviemanagement.services.validator;
+
+import com.bycorp.moviemanagement.exception.InvalidPasswordException;
+import org.springframework.util.StringUtils;
+
+public class PasswordValidator {
+
+    public static void validatePassword(String password, String confirmPassword) {
+        if(!StringUtils.hasText(password) || !StringUtils.hasText(confirmPassword)) {
+            throw new IllegalArgumentException("Password and confirm password are required");
+        }
+
+        if(!password.equals(confirmPassword)) {
+            throw new InvalidPasswordException(password, confirmPassword,"Passwords do not match");
+        }
+
+        if(!containsNumber(password)){
+            throw new InvalidPasswordException(password, "Password must contains at least one number");
+        }
+
+        if(!containsUperCase(password)){
+            throw new InvalidPasswordException(password, "Password must contains at least one uppercase letter");
+        }
+
+        if(!containsLowerCase(password)){
+            throw new InvalidPasswordException(password, "Password must contains at least one lowercase letter");
+        }
+
+        if(!containsSpecialCharacter(password)){
+            throw new InvalidPasswordException(password, "Password must contains at least one special character");
+        }
+    }
+
+    private static boolean containsSpecialCharacter(String password) {
+        return password.matches(".*[!@#$%^&*()_+\\-=\\[\\]{};':\",.<>/?].*");
+    }
+
+    private static boolean containsLowerCase(String password) {
+        return password.matches(".*[a-z].*");
+    }
+
+    private static boolean containsUperCase(String password) {
+        return password.matches(".*[A-Z].*");
+    }
+
+    private static boolean containsNumber(String password) {
+        return password.matches(".*\\d.*");
+    }
+}
