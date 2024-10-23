6133b29c815daeb463df0007c7372233b41f3a51
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2024-10-23 14:18:54 -0500

    quitando bloques try - catch, para usar el manejador de excepciones

--- src/main/java/com/bycorp/moviemanagement/controller/MovieController.java
+++ src/main/java/com/bycorp/moviemanagement/controller/MovieController.java
@@ -1,18 +1,29 @@
 package com.bycorp.moviemanagement.controller;
 
 import com.bycorp.moviemanagement.dto.request.SaveMovie;
+import com.bycorp.moviemanagement.dto.response.ApiError;
 import com.bycorp.moviemanagement.dto.response.GetMovie;
+import com.bycorp.moviemanagement.exception.InvalidPasswordException;
 import com.bycorp.moviemanagement.exception.ObjectNotFoundException;
 import com.bycorp.moviemanagement.services.MovieService;
 import com.bycorp.moviemanagement.utils.MovieGenre;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.net.URI;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.List;
 
 @RestController //combina @Controller y @ResponseBody
@@ -54,7 +65,6 @@ public class MovieController {
     public ResponseEntity<GetMovie> createOneMovie(@Valid @RequestBody SaveMovie movieDto,
                                                 HttpServletRequest request) {
 
-//        System.out.println("fecha: "+movieDto.availabilityEndTime());
         GetMovie movieCreated = movieService.createOne(movieDto);
         String baseUrl = request.getRequestURL().toString();
         URI newLocation = URI.create(baseUrl+"/"+movieCreated.id());
@@ -66,21 +76,13 @@ public class MovieController {
 
     @PutMapping(value = "/{idPelicula}")
     public ResponseEntity<GetMovie> updateOneMovieById(@PathVariable Long idPelicula, @RequestBody @Valid SaveMovie movieDto) {
-        try {
-            GetMovie updatedMovie = movieService.updateOneById(idPelicula, movieDto);
-            return ResponseEntity.ok(updatedMovie);
-        } catch(ObjectNotFoundException e) {
-            return ResponseEntity.notFound().build();
-        }
+        GetMovie updatedMovie = movieService.updateOneById(idPelicula, movieDto);
+        return ResponseEntity.ok(updatedMovie);
     }
 
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<Void> deleteOneMovieById(@PathVariable Long id) {
-        try{
-            movieService.deleteOneById(id);
-            return  ResponseEntity.noContent().build();
-        } catch (ObjectNotFoundException e){
-            return ResponseEntity.notFound().build();
-        }
+        movieService.deleteOneById(id);
+        return  ResponseEntity.noContent().build();
     }
 }
