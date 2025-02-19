8d56e981561aab91738409ba6ba5cc6b60dd7347
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-02-19 18:41:05 -0500

    fix import

--- src/main/java/com/bycorp/moviemanagement/repository/MovieRepository.java
+++ src/main/java/com/bycorp/moviemanagement/repository/MovieRepository.java
@@ -1,12 +1,9 @@
 package com.bycorp.moviemanagement.repository;
 
 import com.bycorp.moviemanagement.entity.Movie;
-import com.bycorp.moviemanagement.utils.MovieGenre;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
 
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long>, JpaSpecificationExecutor<Movie> {
