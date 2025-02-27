7e271c7773659c9eb8d885a05647a4756a8b01f4
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-02-26 23:19:32 -0500

    quitando metodos que no iban en el repositorio

--- src/main/java/com/bycorp/moviemanagement/repository/MovieRepository.java
+++ src/main/java/com/bycorp/moviemanagement/repository/MovieRepository.java
@@ -1,7 +1,5 @@
 package com.bycorp.moviemanagement.repository;
 
-import com.bycorp.moviemanagement.dto.request.SaveMovie;
-import com.bycorp.moviemanagement.dto.response.GetMovie;
 import com.bycorp.moviemanagement.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
@@ -12,7 +10,4 @@ import java.util.Optional;
 @Repository
 public interface MovieRepository extends JpaRepository<Movie, Long>, JpaSpecificationExecutor<Movie> {
     Optional<Movie> findOneById(Long id);
-    Movie createOne(Movie movie);
-    Movie updateOneById(Long id,Movie movie);
-
 }
