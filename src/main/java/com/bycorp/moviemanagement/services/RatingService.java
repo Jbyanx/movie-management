63311baae8d81229caabd3779f8e589d110c96c4
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-02-28 10:59:06 -0500

    cambiando el valor de retorno de GetCompleteRating a GetMovie.GetRating que contiene menos datos

--- src/main/java/com/bycorp/moviemanagement/services/RatingService.java
+++ src/main/java/com/bycorp/moviemanagement/services/RatingService.java
@@ -2,12 +2,15 @@ package com.bycorp.moviemanagement.services;
 
 import com.bycorp.moviemanagement.dto.request.SaveRating;
 import com.bycorp.moviemanagement.dto.response.GetCompleteRating;
+import com.bycorp.moviemanagement.dto.response.GetMovie;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
 public interface RatingService {
     Page<GetCompleteRating> findAll(Pageable pageable);
-    Page<GetCompleteRating> findRatingsByMovieId(Long movieId, Pageable pageable);
+
+    Page<GetMovie.GetRating> findRatingsByMovieId(Long movieId, Pageable pageable);
+
     Page<GetCompleteRating> findRatingsByUserUsername(String user_username, Pageable pageable);
     GetCompleteRating findOneById(Long id);
     GetCompleteRating createOne(SaveRating rating);
