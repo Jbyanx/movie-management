d14b5be8bbf590f96daeefdad054ec7f96e4911c
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-02-28 10:21:26 -0500

    aplicando paginacion y ordenamiento

--- src/main/java/com/bycorp/moviemanagement/services/RatingService.java
+++ src/main/java/com/bycorp/moviemanagement/services/RatingService.java
@@ -1,15 +1,19 @@
 package com.bycorp.moviemanagement.services;
 
+import com.bycorp.moviemanagement.dto.request.SaveRating;
+import com.bycorp.moviemanagement.dto.response.GetCompleteRating;
 import com.bycorp.moviemanagement.entity.Rating;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface RatingService {
-    List<Rating> findAll();
-    List<Rating> findRatingsByMovieId(Long movieId);
-    List<Rating> findRatingsByUserUsername(String user_username);
-    Rating findOneById(Long id);
-    Rating createOne(Rating rating);
-    Rating updateOneById(Long id,Rating rating);
+    Page<GetCompleteRating> findAll(Pageable pageable);
+    Page<GetCompleteRating> findRatingsByMovieId(Long movieId, Pageable pageable);
+    Page<GetCompleteRating> findRatingsByUserUsername(String user_username, Pageable pageable);
+    GetCompleteRating findOneById(Long id);
+    GetCompleteRating createOne(SaveRating rating);
+    GetCompleteRating updateOneById(Long id,SaveRating rating);
     void deleteOneById(Long id);
 }
