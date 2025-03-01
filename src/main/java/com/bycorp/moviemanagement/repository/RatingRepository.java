4e10e52a3bf7ae5eb2375ab71c788e44b2de569f
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-03-01 15:39:37 -0500

    creando querys necesarias para los casos de uso:
    - total de ratings de un usuario
    - promedio de ratings de un usuario
    - rating minimo de un usuario
    - rating maximo de un usuario
    
    todo esto, ya sea pasando el id o el username

--- src/main/java/com/bycorp/moviemanagement/repository/RatingRepository.java
+++ src/main/java/com/bycorp/moviemanagement/repository/RatingRepository.java
@@ -16,6 +16,7 @@ public interface RatingRepository extends JpaRepository<Rating, Long> {
 
     Boolean existsByMovieIdAndUserUsername(Long movieId, String username);
 
+    /*CAOS DE USO PARA MOVIES*/
     @Query("select count(r) from Rating r where r.movieId = ?1")
     Integer countByMovieId(Long id);
 
@@ -27,4 +28,30 @@ public interface RatingRepository extends JpaRepository<Rating, Long> {
 
     @Query("select max(r.rating) from Rating r where r.movieId = ?1")
     Integer maxRatingByMovieId(Long id);
+
+    /*CASOS DE USO PARA USERS*/
+
+    @Query("select count(r) from Rating r where r.user.username = ?1")
+    Integer countRatingsByUserUsername(String username);
+
+    @Query("select avg(r.rating) from Rating r where r.user.username = ?1")
+    Double avgRatingByUserUsername(String username);
+
+    @Query("select min(r.rating) from Rating r where r.user.username = ?1")
+    Integer minRatingByUserUsername(String username);
+
+    @Query("select max(r.rating) from Rating r where r.user.username = ?1")
+    Integer maxRatingByUserUsername(String username);
+
+    @Query("select count(r) from Rating r where r.userId = ?1")
+    Integer countRatingsByUserId(Long id);
+
+    @Query("select avg(r.rating) from Rating r where r.userId = ?1")
+    Double avgRatingByUserId(Long id);
+
+    @Query("select min(r.rating) from Rating r where r.userId = ?1")
+    Integer minRatingByUserId(Long id);
+
+    @Query("select max(r.rating) from Rating r where r.userId = ?1")
+    Integer maxRatingByUserId(Long id);
 }
