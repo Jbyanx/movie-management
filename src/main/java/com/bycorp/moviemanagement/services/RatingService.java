bdab0362c99d1773c0bcd7fe8b284b17f7505ab2
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-03-01 11:18:49 -0500

    cambiando el valor de retorno de findRatingsByUserUsername de un GetCompleteRating a solamente un GetUser.GetRating que es un rating optimizado para los usuarios

--- src/main/java/com/bycorp/moviemanagement/services/RatingService.java
+++ src/main/java/com/bycorp/moviemanagement/services/RatingService.java
@@ -3,6 +3,7 @@ package com.bycorp.moviemanagement.services;
 import com.bycorp.moviemanagement.dto.request.SaveRating;
 import com.bycorp.moviemanagement.dto.response.GetCompleteRating;
 import com.bycorp.moviemanagement.dto.response.GetMovie;
+import com.bycorp.moviemanagement.dto.response.GetUser;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
@@ -11,7 +12,8 @@ public interface RatingService {
 
     Page<GetMovie.GetRating> findRatingsByMovieId(Long movieId, Pageable pageable);
 
-    Page<GetCompleteRating> findRatingsByUserUsername(String user_username, Pageable pageable);
+    Page<GetUser.GetRating> findRatingsByUserUsername(String user_username, Pageable pageable);
+
     GetCompleteRating findOneById(Long id);
     GetCompleteRating createOne(SaveRating rating);
     GetCompleteRating updateOneById(Long id,SaveRating rating);
