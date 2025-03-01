e2d7081ae4e2a175c0b52ebb754b3139b3d8d987
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-03-01 14:52:56 -0500

    creando querys para los nuevos campos del GetMovieMovie Statistic - cantidad de ratings de una pelicula - promedio de ratings de una pelicula - rating minimo de una pelicula - rating maximo de una pelicula

--- src/main/java/com/bycorp/moviemanagement/repository/RatingRepository.java
+++ src/main/java/com/bycorp/moviemanagement/repository/RatingRepository.java
@@ -4,6 +4,7 @@ import com.bycorp.moviemanagement.entity.Rating;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -14,4 +15,16 @@ public interface RatingRepository extends JpaRepository<Rating, Long> {
     Page<Rating> findRatingsByUserUsername(String user_username, Pageable pageable);
 
     Boolean existsByMovieIdAndUserUsername(Long movieId, String username);
+
+    @Query("select count(r) from Rating r where r.movieId = ?1")
+    Integer countByMovieId(Long id);
+
+    @Query("select avg(r.rating) from Rating r where r.movieId = ?1")
+    Double avgRatingByMovieId(Long id);
+
+    @Query("select min(r.rating) from Rating r where r.movieId = ?1")
+    Integer minRatingByMovieId(Long id);
+
+    @Query("select max(r.rating) from Rating r where r.movieId = ?1")
+    Integer maxRatingByMovieId(Long id);
 }
