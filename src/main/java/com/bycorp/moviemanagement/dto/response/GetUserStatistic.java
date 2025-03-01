b3003fcaf76570e06c6446d6f162cc97465010c1
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-03-01 14:19:29 -0500

    creando dto para el detalle de usuarios, con mas informacion

---
+++ src/main/java/com/bycorp/moviemanagement/dto/response/GetUserStatistic.java
@@ -0,0 +1,14 @@
+package com.bycorp.moviemanagement.dto.response;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+public record GetUserStatistic(
+    String username,
+    LocalDateTime createdAt,
+    int totalRatings,
+    double averageRating,
+    int lowestRating,
+    int highestRating
+) implements Serializable {
+}
