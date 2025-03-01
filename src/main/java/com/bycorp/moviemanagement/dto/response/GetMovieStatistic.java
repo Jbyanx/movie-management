c5d1f95adf1aa0a5b772365391e64883f5d54000
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-03-01 15:41:27 -0500

    - usando snake_case para el json de respuesta

--- src/main/java/com/bycorp/moviemanagement/dto/response/GetMovieStatistic.java
+++ src/main/java/com/bycorp/moviemanagement/dto/response/GetMovieStatistic.java
@@ -1,5 +1,7 @@
 package com.bycorp.moviemanagement.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.io.Serializable;
 
 public record GetMovieStatistic(
@@ -7,10 +9,15 @@ public record GetMovieStatistic(
         String title,
         String director,
         String genre,
+        @JsonProperty("total_ratings")
         int totalRatings,
+        @JsonProperty("release_year")
         int releaseYear,
+        @JsonProperty("average_rating")
         double averageRating,
+        @JsonProperty("lowest_rating")
         int lowestRating,
+        @JsonProperty("highest_rating")
         int highestRating
 ) implements Serializable {
 }
