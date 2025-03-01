50846ca0756834e81c47daae567e27cfd5aa00ea
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-03-01 11:21:13 -0500

    quitando el mapeo de la lista de ratings del dto de usuario y cambiandola por el total de pelicuals calificadas

--- src/main/java/com/bycorp/moviemanagement/mapper/UserMapper.java
+++ src/main/java/com/bycorp/moviemanagement/mapper/UserMapper.java
@@ -11,10 +11,12 @@ public class UserMapper {
     public static GetUser toGetDto(User entity) {
         if(entity == null) return null;
 
+        int totalRatedMovies = entity.getRatings() != null ? entity.getRatings().size() : 0;
+
         return new GetUser(
                 entity.getUsername(),
                 entity.getName(),
-                RatingMapper.toGetUserRatingDtoList(entity.getRatings())
+                totalRatedMovies
         );
     }
 
