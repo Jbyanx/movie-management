099bbbd17313290242f9eaf356a011f4f35edead
Jabesbyanx <108432086+Jbyanx@users.noreply.github.com>
2025-03-01 12:41:13 -0500

    creando metodo para mapear entidad Movie a GetMovieStatistic usando streams para hacer los calculos de las variables

--- src/main/java/com/bycorp/moviemanagement/mapper/MovieMapper.java
+++ src/main/java/com/bycorp/moviemanagement/mapper/MovieMapper.java
@@ -2,7 +2,9 @@ package com.bycorp.moviemanagement.mapper;
 
 import com.bycorp.moviemanagement.dto.request.SaveMovie;
 import com.bycorp.moviemanagement.dto.response.GetMovie;
+import com.bycorp.moviemanagement.dto.response.GetMovieStatistic;
 import com.bycorp.moviemanagement.entity.Movie;
+import com.bycorp.moviemanagement.entity.Rating;
 
 import java.util.List;
 
@@ -48,5 +50,34 @@ public class MovieMapper {
         oldMovie.setGenre(movieDto.genre());
         oldMovie.setDirector(movieDto.director());
         oldMovie.setReleaseYear(movieDto.releaseYear());
+    }
+
+    public static GetMovieStatistic toGetStatisticDto(Movie movie) {
+        if(movie == null) return null;
+
+        int totalRatings = movie.getRatings() != null ? movie.getRatings().size() : 0;
+
+        int sumaRatings = movie.getRatings() != null ?
+                movie.getRatings().stream().mapToInt(Rating::getRating).sum() : 0;
+
+        int lowestRating = movie.getRatings() != null ?
+                movie.getRatings().stream().map(Rating::getRating).min((Integer::compare)).orElse(0) : 0;
+
+        int highestRating = movie.getRatings() != null ?
+                movie.getRatings().stream().map(Rating::getRating).max((Integer::compare)).orElse(0) : 0;
+
+        double averageRating = movie.getRatings() != null ? (double) sumaRatings / totalRatings : 0;
+
+        return new GetMovieStatistic(
+                movie.getId(),
+                movie.getTitle(),
+                movie.getDirector(),
+                movie.getGenre().name(),
+                totalRatings,
+                movie.getReleaseYear(),
+                averageRating,
+                lowestRating,
+                highestRating
+        );
     }
 }
